5be019ac56d3188218454422a321f8133e7c11fe
h0wzy <h0wzymarcos@gmail.com>
2025-11-01 09:05:52 -0300

    feat: add refactor structure and add basicInfo

--- server.go
+++ server.go
@@ -10,7 +10,8 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/lru"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/H0wZy/go.learning/graph"
+	"github.com/H0wZy/go.learning/generated"
+	"github.com/H0wZy/go.learning/resolvers"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
@@ -22,7 +23,7 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.New(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{}}))
 
 	srv.AddTransport(transport.Options{})
 	srv.AddTransport(transport.GET{})
